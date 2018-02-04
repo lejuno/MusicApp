7f66eea269524b85a24343b3fc9b7d053b3fad62
lejuno <keur_ljn@hotmail.com>
2018-02-04 12:47:16 -0200

    20180204

--- app/src/main/java/com/example/android/musicapp/MySongsActivity.java
+++ app/src/main/java/com/example/android/musicapp/MySongsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MySongsActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MySongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_songs);
+
+        // Should Send Action to Detail View with specific Song Names
+        setupClickListener(R.id.id_item_1, DetailedSongActivity.class);
+        setupClickListener(R.id.id_item_2, DetailedSongActivity.class);
+        setupClickListener(R.id.id_item_3, DetailedSongActivity.class);
+        setupClickListener(R.id.id_item_4, DetailedSongActivity.class);
+        setupClickListener(R.id.id_item_5, DetailedSongActivity.class);
+        setupClickListener(R.id.id_item_6, DetailedSongActivity.class);
+
+        // Should Send Action to Play View with specific Song Names
+        setupClickListener(R.id.id_play_item_1, DetailedSongActivity.class);
+        setupClickListener(R.id.id_play_item_2, DetailedSongActivity.class);
+        setupClickListener(R.id.id_play_item_3, DetailedSongActivity.class);
+        setupClickListener(R.id.id_play_item_4, DetailedSongActivity.class);
+        setupClickListener(R.id.id_play_item_5, DetailedSongActivity.class);
+        setupClickListener(R.id.id_play_item_6, DetailedSongActivity.class);
+
+
+
+        // Buttons
+        setupClickListener(R.id.id_button_return, MainActivity.class);
+        setupClickListener(R.id.id_button_search, RandomSongFinderActivity.class);
+    }
+
+    private void setupClickListener(int id, final Class<? extends Activity> clazz) {
+        View view = findViewById(id);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MySongsActivity.this, clazz);
+                startActivity(intent);
+            }
+        });
     }
 }
